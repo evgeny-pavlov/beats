db3ba6d50814fc2d5810b6cc7bfd88da490a5bf6
Evgeny Pavlov <soulqd@gmail.com>
2021-06-05 01:07:59 +0300

    собрано в gulp

---
+++ gulp.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    SRC_PATH : "src",
+    DIST_PATH : "dist",
+    STYLES_LIBS : [
+        "node_modules/jquery/dist/jquery.min.js"
+    ],
+    JS_LIBS : [
+        
+    ]
+}
